874c49d8cdd8ec6ff3bf3fe9b25d8b40aeb651fb
lukasboss <bosslu01@thu.de>
2026-02-18 09:02:51 +0100

    test v15

--- sample_project/main/main.c
+++ sample_project/main/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "version.h"
 
 void app_main(void)
 {
     printf("Hello world!\n");
+    printf("Version: %s\n", GIT_VERSION);
     // Testing the workflow ...
     //  Second Test
     // Third Test
@@ -10,4 +12,6 @@ void app_main(void)
     // Fifth Test
     // Sixth Test
     // Seventh Test
+    // Eighth Test
+
 }
