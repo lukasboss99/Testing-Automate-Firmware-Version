2abe471577204a8595beb12be69d5f493e136067
ALKO-SEIBOLDA <alexander.seibold@alko-tech.com>
2026-02-19 16:38:46 +0100

    Updated Script
    Translatet error messages to english and added Start_Installation.bat

--- install/Install.ps1
+++ install/Install.ps1
@@ -3,27 +3,29 @@
     return ($LASTEXITCODE -eq 0)
 }
 
-
-
 if (-not (Test-GitInstalled)) {
-    Write-Host "Git ist nicht installiert."
-    Write-Host "Installiere Git"
-    winget install --id Git.Git -e --source winget
+    Write-Host "Git is not installed."
+    Write-Host "Installing Git"
+    winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements
     
-    Write-Host "Git installiert, starte Powershell neu"
+    Write-Host "Git installed, Restart Powershell script"
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
     exit
 }
 
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
-    Write-Host "Git ist installiert, aber noch nicht im PATH."
-    exit
+    Write-Host "Git ist installed, but in the wrong PATH."
+    exit 1
 }
+Write-Host "Git installed correctly"
 
-Write-Host "Git ist korrekt installiert"
-Get-Location
-cd ..
-$currentPath = Get-Location
-Write-Host $currentPath
+Set-Location ..
+$currentPath = (Get-Location).Path
+Write-Host "Execution in: $currentPath"
 git config --local core.hooksPath .githooks
-Start-Sleep -Seconds 5
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Failed to set core.hooksPath."
+    exit 1
+}
+Write-Host "Githooks installed succesfully"
+Start-Sleep -Seconds 3
